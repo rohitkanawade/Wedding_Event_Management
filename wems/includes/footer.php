21c8de4df2ecd1828c1446258c6a880bf0d0125d
Rupali Gopale <rohitkanawade91@gmail.com>
2023-01-11 18:51:24 +0530

    first commit

---
+++ wems/includes/footer.php
@@ -0,0 +1,8 @@
+    
+    <!-- copyright -->
+    <div class="agileits-w3layouts">
+        <div class="container">
+            <p>© 2022 Wedding Event Management Sysyem. All rights reserved | </p>
+        </div>
+    </div>
+    <!-- //copyright -->
